d04bbf7af66d213d9138ba6e34b91a999a731082
Arthur Osipyan <arthurosipyan97@gmail.com>
2019-12-04 11:51:40 -0500

    updates

---
+++ movies_search/build/precache-manifest.714ff451dbcae6f3a87d0ad21b9ba5a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac7cf21e7891c8196e320a39315c28e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0177a7c71197c5054db0",
+    "url": "/static/css/main.830782f4.chunk.css"
+  },
+  {
+    "revision": "7cb1ee982b199b426734",
+    "url": "/static/js/2.16783803.chunk.js"
+  },
+  {
+    "revision": "0177a7c71197c5054db0",
+    "url": "/static/js/main.2bd13947.chunk.js"
+  },
+  {
+    "revision": "8859cbc1055979a09b44",
+    "url": "/static/js/runtime-main.2d5333f3.js"
+  }
+]);
